e70fe49b58a03ecd014d5503d4a90045cd872572
tharun <tharun24tharun@gmail.com>
2025-09-30 12:34:30 +0530

    add initial Dart and Flutter configuration files

---
+++ day1.dart
@@ -0,0 +1,27 @@
+void main(){
+    print('Hello World');
+    //Variables
+    var name="THARUN"; //var
+    final country ="INDIA"; //final
+    const pi= 3.14159; //constant
+
+    //data types
+    int age = 17; //integer
+    double height = 5.3; // double(in decimal)
+    String city = "Chennai";//only text
+    bool something= true;//true or false
+
+    List<String> animals=["Dog","Cat"];// like array only able to give same kind of data type
+    Map<String,int> marks = {"Chemistry":30,"DT":33,"PSC":40};//like dictionary in python
+
+    //output
+    print("Name : $name");
+    print("Country : $country");
+    print("Pi : $pi");
+    print("Age : $age");
+    print("Height : $height");
+    print("City : $city");
+    print("TRUE/FALSE : $something");
+    print("List : $animals");
+    print("Map : $marks")
+}
